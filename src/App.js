9a1f585974ad3c9f8948edceec41c705e14a76be
martine9696 <gogulnithin07>
2024-05-16 22:34:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import styles from "./App.module.css";
+export default function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const arr = [
+    [7, 8, 9, "+"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "*"],
+    ["C", 0, "=", "/"],
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles.container}>
+        <h1>React Calculator</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput((c) => c + e.target.value)}
+        />
+        <div>{output}</div>
+        <div className={styles.buttons}>
+          {arr.flat().map((val, index) => {
+            return (
+              <Button
+                key={crypto.randomUUID()}
+                setOutput={setOutput}
+                text={val}
+                input={input}
+                setInput={setInput}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
+function Button({ text, input, setInput, setOutput }) {
+  let inputVal;
+  function handleClick(e) {
+    inputVal = e.target.innerText;
+    if (inputVal === "=" && input.length === 0) {
+      setOutput("Error");
+    } else {
+      if (inputVal === "C") {
+        setInput((c) => "");
+        setOutput((c) => "");
+      } else if (inputVal === "=") {
+        setOutput((c) => eval(input));
+      } else {
+        if (
+          ["+", "-", "*", "/"].includes(input.charAt(input.length - 1)) &&
+          isNaN(inputVal)
+        ) {
+        } else {
+          setInput((c) => c + inputVal);
+        }
+      }
+    }
+  }
+  return (
+    <button onClick={(e) => handleClick(e)} className={styles.btn}>
+      {text}
+    </button>
+  );
+}
